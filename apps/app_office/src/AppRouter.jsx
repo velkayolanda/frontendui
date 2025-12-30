946541f84e4b000086d203471a106d256e7ea5a2
Hrbolek <profesor@hrbolek.cz>
2025-12-30 21:44:44 +0100

    introduction of app_office and package

---
+++ apps/app_office/src/AppRouter.jsx
@@ -0,0 +1,46 @@
+import {
+    createBrowserRouter,
+    Outlet,
+    RouterProvider,
+} from "react-router-dom";
+import { NavigationHistoryLinks, NavigationHistoryProvider } from '../../../packages/_template/src/Base/Helpers/NavigationHistoryProvider';
+
+import { BaseRouterSegments } from "../../../packages/_template/src/Base/Pages/RouterSegment";
+
+import { GroupRouterSegments } from "../../../packages/_template/src/GroupGQLModel/Pages/RouterSegment";
+import { RoleTypeRouterSegments } from "../../../packages/_template/src/RoleTypeGQLModel/Pages";
+import { UserRouterSegments } from "../../../packages/_template/src/UserGQLModel/Pages/RouterSegment";
+import { GroupTypeRouterSegments } from "../../../packages/_template/src/GroupTypeGQLModel/Pages/RouterSegment";
+import { RoleRouterSegments } from "../../../packages/_template/src/RoleGQLModel/Pages";
+import { AppNavbar } from "./AppNavbar";
+
+
+const AppLayout = () => (
+    <NavigationHistoryProvider>
+        <AppNavbar />
+        <NavigationHistoryLinks />
+        <Outlet />
+    </NavigationHistoryProvider>
+);
+
+const Routes = [
+    {
+        path: "/",          // root
+        element: <AppLayout />,
+        children: [
+            ...BaseRouterSegments,
+            ...GroupRouterSegments,
+            ...RoleTypeRouterSegments,
+            ...UserRouterSegments,
+            ...GroupTypeRouterSegments,
+            ...RoleRouterSegments,
+        ],
+    },
+];
+
+// console.log("Routes", Routes)
+// console.log("Routes", GroupRouterSegments)
+
+const router = createBrowserRouter(Routes);
+
+export const AppRouter = () => <RouterProvider router={router} />;
---
+++ packages/office/src/index.js
@@ -0,0 +1,7 @@
+// export * as GroupGQLModel from "./GroupGQLModel"
+// export * as UserGQLModel from "./UserGQLModel"
+// export * as RoleGQLModel from "./RoleGQLModel"
+// export * as MembershipGQLModel from "./MembershipGQLModel"
+
+// export * as GroupTypeGQLModel from "./GroupTypeGQLModel"
+// export * as RoleTypeGQLModel from "./RoleTypeGQLModel"
